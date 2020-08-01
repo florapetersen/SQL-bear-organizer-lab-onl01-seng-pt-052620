da626610307f608064400f9700336ac37d251cec
Flora Petersen <floramarina@comcast.net>
2020-08-01 17:53:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Mr. Chocolate", 6, "male", "brown", "happy", yes);
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Rowdy", 3, "male", "white", "excited", yes);
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Tabitha", 5, "female", "brown", "silly", yes);
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Sergeant Brown", 9, "female", "purple", "jolly", yes);
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Melissa", 1, "female", "black", "confused", yes);
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Grinch", 12, "male", "green", "annoyed", yes);
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Wendy", 3, "female", "blue", "hyper", yes);
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
+INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES (NULL, 6, "male", "white", "erratic", yes);
